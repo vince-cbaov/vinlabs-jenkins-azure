9f8e39d46f94ec3fa0c0e7d3f065c2e42bb37128
vince-cbaov <vfalconer@hotmail.co.uk>
2025-11-09 22:16:22 +0000

    Update Flask route to render index.html

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # app.py
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Docker!"
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(debug=True)
+
